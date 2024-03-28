49154ba1ac3c679c7897fc8000d61bd9001700f2
Enrique Comesaña <enriquecbar@gmail.com>
2024-03-28 11:27:28 +0100

    menu items map

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -1,83 +1,61 @@
 import { Menu, Transition } from '@headlessui/react';
+import { MENU_ITEMS } from '../constants/menu';
 
 const NavMenu = () => {
   return (
     <nav>
       <ul className='hidden lg:flex lg:space-x-7 lg:ml-14'>
-        <li>
-          <Menu>
-            <Menu.Button>
-              Features{' '}
-              <img
-                src='images/icon-arrow-down.svg'
-                alt='Icon down arrow'
-                className='inline-block ml-1'
-              />
-            </Menu.Button>
-            <Transition
-              enter='transition duration-100 ease-out'
-              enterFrom='transform scale-95 opacity-0'
-              enterTo='transform scale-100 opacity-100'
-              leave='transition duration-75 ease-out'
-              leaveFrom='transform scale-100 opacity-100'
-              leaveTo='transform scale-95 opacity-0'
-            >
-              <Menu.Items className='absolute mt-4 w-40 py-3 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none'>
-                <Menu.Item>
-                  {({ active }) => (
-                    <a
-                      className={`${
-                        active && 'bg-gray-100 text-gray-900'
-                      } block w-full text-left px-4 py-2 text-sm cursor-pointer`}
-                      href='/account-settings'
-                    >
-                      <img
-                        src='images/icon-calendar.svg'
-                        className='inline w-4 mr-3'
-                        alt='Calendar Icon'
-                      />
-                      Calendar
-                    </a>
-                  )}
-                </Menu.Item>
-                <Menu.Item>
-                  {({ active }) => (
-                    <a
-                      className={`${
-                        active && 'bg-gray-100 text-gray-900'
-                      } block w-full text-left px-4 py-2 text-sm cursor-pointer`}
-                      href='/account-settings'
-                    >
-                      <img
-                        src='images/icon-reminders.svg'
-                        className='inline w-4 mr-3'
-                        alt='Reminders Icon'
-                      />
-                      Reminders
-                    </a>
-                  )}
-                </Menu.Item>
-                <Menu.Item>
-                  {({ active }) => (
-                    <a
-                      className={`${
-                        active && 'bg-gray-100 text-gray-900'
-                      } block w-full text-left px-4 py-2 text-sm cursor-pointer`}
-                      href='/account-settings'
-                    >
-                      <img
-                        src='images/icon-planning.svg'
-                        className='inline w-4 mr-3'
-                        alt='Planning Icon'
-                      />
-                      Planning
-                    </a>
-                  )}
-                </Menu.Item>
-              </Menu.Items>
-            </Transition>
-          </Menu>
-        </li>
+        {MENU_ITEMS.map(menuItem => (
+          <li key={menuItem.title}>
+            <Menu>
+              <Menu.Button>
+                {menuItem.title}{' '}
+                {menuItem.subItems && (
+                  <img
+                    src='images/icon-arrow-down.svg'
+                    alt='Icon down arrow'
+                    className='inline-block ml-1'
+                  />
+                )}
+              </Menu.Button>
+              <Transition
+                enter='transition duration-100 ease-out'
+                enterFrom='transform scale-95 opacity-0'
+                enterTo='transform scale-100 opacity-100'
+                leave='transition duration-75 ease-out'
+                leaveFrom='transform scale-100 opacity-100'
+                leaveTo='transform scale-95 opacity-0'
+              >
+                {/* Renderizar Menu.Items solo si menuItem tiene subItems */}
+                {menuItem.subItems && (
+                  <Menu.Items className='absolute mt-4 w-40 py-3 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none'>
+                    {menuItem.subItems.map(subItem => (
+                      <Menu.Item key={subItem.id}>
+                        {({ active }) => (
+                          <a
+                            className={`${
+                              active && 'bg-gray-100 text-gray-900'
+                            } block w-full text-left px-4 py-2 text-sm cursor-pointer`}
+                            href={subItem.link}
+                          >
+                            {subItem.icon && (
+                              <img
+                                src={subItem.icon}
+                                className='inline w-4 mr-3'
+                                alt={` ${subItem.title} Icon`}
+                              />
+                            )}
+                            {subItem.title}
+                          </a>
+                        )}
+                      </Menu.Item>
+                    ))}
+                  </Menu.Items>
+                )}
+              </Transition>
+            </Menu>
+          </li>
+        ))}
       </ul>
     </nav>
   );
--- src/constants/menu.tsx
+++ src/constants/menu.tsx
@@ -1,30 +1,33 @@
+import { v4 } from 'uuid';
+
 interface MenuItem {
+  id: string;
   title: string;
   link?: string;
   icon?: string;
   subItems?: MenuItem[];
 }
 
-export const MENU: MenuItem[] = [
+export const MENU_ITEMS: MenuItem[] = [
   {
+    id: v4(),
     title: 'Features',
     subItems: [
+      { id: v4(), title: 'Todo list', link: '', icon: 'images/icon-todo.svg' },
       {
-        title: 'Todo list',
-        link: '',
-        icon: 'images/icon-todo.svg'
-      },
-      {
+        id: v4(),
         title: 'Calendar',
         link: '',
         icon: 'images/icon-calendar.svg'
       },
       {
+        id: v4(),
         title: 'Reminders',
         link: '',
         icon: 'images/icon-reminders.svg'
       },
       {
+        id: v4(),
         title: 'Planning',
         link: '',
         icon: 'images/icon-planning.svg'
@@ -32,25 +35,14 @@ export const MENU: MenuItem[] = [
     ]
   },
   {
+    id: v4(),
     title: 'Company',
     subItems: [
-      {
-        title: 'History',
-        link: '',
-        icon: ''
-      },
-      {
-        title: 'Our Team',
-        link: '',
-        icon: ''
-      },
-      {
-        title: 'Blog',
-        link: '',
-        icon: ''
-      }
+      { id: v4(), title: 'History', link: '', icon: '' },
+      { id: v4(), title: 'Our Team', link: '', icon: '' },
+      { id: v4(), title: 'Blog', link: '', icon: '' }
     ]
   },
-  { title: 'Careers', link: '' },
-  { title: 'About', link: '' }
+  { id: v4(), title: 'Careers', link: '' },
+  { id: v4(), title: 'About', link: '' }
 ];
